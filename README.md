2d188aa15e56d83a2361b276cb4677f5ad6732c7
Asap Jay <AsapJayfroshcr9@gmail.com>
2021-03-03 16:07:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# overlay_hud
+
+Provides a loading overlay for async calls.
+
+
+## Usage 
+#### Add import statement
+``` dart
+    import 'package:overlay_hud/overlay_hud';
+    
+```
+
+#### Wrap your widget/screen in an OverlayHUD widget
+``` dart
+     OverlayHUD(
+      child: SomeWidget(),
+    );
+    
+```
---
+++ lib/src/overlay_progress_hud.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+///
+/// Wrap around any widget that makes an async call to show a modal progress
+/// indicator while the async call is in progress.
+///
+/// The progress indicator can be turned on or off using [inAsyncCall]
+///
+/// The progress indicator defaults to a [CircularProgressIndicator] but can be
+/// any kind of widget
+///
+/// The progress indicator can be positioned using [offset] otherwise it is
+/// centered
+///
+/// The modal barrier can be dismissed using [dismissible]
+///
+/// The color of the modal barrier can be set using [color]
+///
+/// The opacity of the modal barrier can be set using [opacity]
+///
+/// HUD=Heads Up Display
+///
+class OverlayHUD extends StatelessWidget {
+  final bool inAsyncCall;
+  final double opacity;
+  final Color color;
+  final Widget progressIndicator;
+  final Offset offset;
+  final bool dismissible;
+  final Widget child;
+
+  OverlayHUD({
+    Key key,
+    @required this.inAsyncCall,
+    this.opacity = 0.2,
+    this.color = Colors.grey,
+    this.progressIndicator = const CupertinoActivityIndicator(
+      animating: true,
+      radius: 14,
+    ),
+    this.offset,
+    this.dismissible = false,
+    @required this.child,
+  })  : assert(child != null),
+        assert(inAsyncCall != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> widgetList = [];
+    widgetList.add(child);
+    if (inAsyncCall) {
+      Widget layOutProgressIndicator;
+      if (offset == null)
+        layOutProgressIndicator = Center(child: progressIndicator);
+      else {
+        layOutProgressIndicator = Positioned(
+          child: progressIndicator,
+          left: offset.dx,
+          top: offset.dy,
+        );
+      }
+      final modal = [
+        new Opacity(
+          child: new ModalBarrier(dismissible: dismissible, color: color),
+          opacity: opacity,
+        ),
+        layOutProgressIndicator
+      ];
+      widgetList += modal;
+    }
+    return WillPopScope(
+      onWillPop: () => Future.value(!inAsyncCall),
+      child: new Stack(
+        children: widgetList,
+      ),
+    );
+  }
+}
